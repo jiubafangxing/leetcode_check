e7bf63c7e7719950811fdbb0a3773e2574a895ce
yunrong <18404903536@163.com>
2024-06-26 15:24:58 +0800

    QuestionLongestSubstringWithoutRepeatingCharacters.java

---
+++ java/QuestionLongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,74 @@
+public class QuestionLongestSubstringWithoutRepeatingCharacters{
+
+	public static int findLengthOfLongestSubstring(String str){
+		char[] testArray = str.toCharArray();
+		int[] indexArray= new int[256];
+		for(int i =0;i<indexArray.length;i++){
+			indexArray[i] = -1;
+		}
+		Integer prevIdx = 0;
+		Integer max = 0;
+		for(int i=0;i< testArray.length;i++){
+			int idx = indexArray[(int)testArray[i]];
+			prevIdx = Math.max(idx,prevIdx);
+			int curLen = i - prevIdx;
+			if(curLen > max){
+				max = curLen;
+			}
+			indexArray[(int)testArray[i]] = i;
+		}
+		return max;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	public static void main(String[] args){
+		String teststr = "abcabcbb";	
+		int result = findLengthOfLongestSubstring(teststr);
+		System.out.println("result is "+ result);
+		teststr =  "bbbbb";
+		result = findLengthOfLongestSubstring(teststr);
+		System.out.println("result is "+ result);
+		teststr = "pwwkew";
+		result = findLengthOfLongestSubstring(teststr);
+		System.out.println("result is "+result);
+	
+	}
+}
