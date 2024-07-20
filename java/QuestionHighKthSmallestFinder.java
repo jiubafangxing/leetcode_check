4e5ab308d07eb9281e761c4aa82979145e68ff71
yunrong <18404903536@163.com>
2024-07-20 12:21:51 +0800

    bfprt

--- java/QuestionHighKthSmallestFinder.java
+++ java/QuestionHighKthSmallestFinder.java
@@ -4,24 +4,32 @@
  *在一个无序数组中，求最小的第k个数，
  */
 public class QuestionHighKthSmallestFinder{
-	public static int find(int[] arr, int start, int end, int limit){
-		if(start > end){
-			return -1;
+	public static int find(int[] arr, int start, int end, int limit, int pos){
+		if(start ==  end){
+			return arr[end];
 		}
-		int midNum= selectNum(arr, start , end, limit);
-		int i = start;
-		int j = end;
-		while(i !=  j){
-		
+		int midNum= selectNum(arr, start , end, limit, pos);
+		int small = start -1;
+		int big = end+1;
+		int cur = start;
+		while(cur != big){
+			if(arr[cur] < midNum){
+				swap(arr, cur++, ++small);
+			}else if(arr[cur] > midNum){
+				swap(arr, cur, --big);
+			}else{
+				cur++;
+			}
 		}
-		arr[i] = midNum;
-
-		if(i == limit-1){
-			return arr[i];
-		}else if (i > limit-1){
-			return find(arr,start, i-1, limit);	
+  		small = small+1;
+		big = big-1;
+		if(limit >= small && limit <= big){
+			return arr[limit];
+		}else if (limit < small){
+			return find(arr, start, small-1, limit, pos);
+				
 		}else{
-			return find(arr, i+1, end, limit);	
+			return find(arr, big+1, end, limit, pos);
 		}
 	}
 
@@ -32,32 +40,19 @@ public class QuestionHighKthSmallestFinder{
 	}
 
 
-	public static int selectNum(int[] arr, int start , int end, int limit){
+	public static int selectNum(int[] arr, int start , int end, int limit, int pos){
 		int size = (end -start +1);
-		if(size <=3){
-			insertSort(arr, start, end);
-			return arr[limit];
-		}
-		int childSize = size/limit;
-		int memLen = size/limit + (size%limit == 0  ? 0: 1);
+		int childSize = size/pos;
+		int memLen = childSize;
+			memLen = childSize + (size%pos == 0  ? 0: 1);
 		int[] memArr = new int[memLen];
 		for(int i=0;i< memLen; i++){
-			int[] childArr = new int[limit];
-			int num = i;
-			int no = 0;
-			while((num+no*childSize) < arr.length && no < childArr.length){
-				int ppos = num+no*childSize;
-				childArr[no] = arr[ppos];
-				no++;
-			}
-			int med = getMedian(childArr,0, no-1);
-			if(med == 12){
-				System.out.println("med:"+med);
-			}
-			memArr[i] = childArr[med];
-
+			int biginI = start + i*pos;
+			int endI = biginI + pos -1;
+			int midIdx = getMedian(arr, biginI,Math.min(endI, end) );
+			memArr[i] = arr[midIdx];
 		}
-		return find(memArr, 0, memLen-1, memLen/2);	
+		return find(memArr, 0, memLen-1, memLen/2,pos);	
 	}
 
 
@@ -75,39 +70,21 @@ public class QuestionHighKthSmallestFinder{
 	public static void insertSort(int[] arr, int start, int end){
 		for(int i=start;i<= end;i++){
 			int tmp = i;
-			while(tmp >start  && arr[tmp-1] >  arr[tmp]){
-				int exc = arr[tmp-1]	;
-				arr[tmp-1] = arr[tmp];
-				arr[tmp] = exc;
+			while(tmp > start  && arr[tmp-1] >  arr[tmp]){
+				swap(arr, tmp-1, tmp);
 				tmp--;
 			}	
 		}
 	}
-	// public static void main(String[] args){
-	// 	int[] arr = {5,4,3,2,1,7,8,6,8,9};	
-	// 	int result = find(arr,0, arr.length-1, 5);
-	// 	System.out.println("最小的值是"+result);
-	// }
-
-
 	public static void main(String[] args){
-		int[] arr = {5,4,3,2,1,7,8,6,8,9};
-		int provit = 7;
-		int small = -1;
-		int cur = 0;
-		int big = arr.length;
-		while(cur != big){
-			if(arr[cur] < provit){
-				swap(arr, ++small, cur++);
-			}else if (arr[cur] > provit){
-				swap(arr, cur, --big);
-			}else{
-				cur++;
-			}
-
-		}
-		for(int i=0;i<arr.length;i++){
-			System.out.println(arr[i]);
-		}
+		int[] arr = {6,4,3,2,1,7};	
+		int limit = 3;
+		int result = find(arr,0, arr.length-1, limit-1,limit-1);
+		System.out.println("最小的值是"+result);
 	}
+
+
+	// public static void main(String[] args){
+	
+	// }
 }
