1cdbb28027ac425798fbb295facb70dddb8debf7
yunrong <18404903536@163.com>
2024-07-29 18:43:49 +0800

    QuestionHighStringRotationChecker.java

---
+++ java/QuestionHighStringRotationChecker.java
@@ -0,0 +1,40 @@
+/*
+ * 给定两个字符串str1和str2，判断str2是不是str1的旋变字符串。
+ * Given two strings str1 and str2, determine whether str2 is a rotation of str1.
+ */
+public class QuestionHighStringRotationChecker {
+
+	public static boolean isRotation(String str1, String str2){
+		if(str1 == null || str2 == null || str1.length() != str2.length()){
+			return false;
+		}
+		boolean result = false;
+		char[] s1Array = str1.toCharArray();
+		char[] s2Array = str2.toCharArray();
+	 	return isRotation(s1Array,s2Array,0,0, s1Array.length);
+	}
+
+
+	public static boolean isRotation(char[] s1Array, char[] s2Array, int start1, int start2, int size){
+		if(size == 1){
+			return s1Array[start1] == s2Array[start2];
+		}
+		boolean result = false;
+		for(int i=1; i<= size-2;i++){
+			if((isRotation(s1Array,s2Array, start1, start1,i)&& isRotation(s1Array,s2Array, start1+ i,start1+i, size-i)) 
+			       	||( isRotation(s1Array, s2Array, start1,start2+i-1,i ) && isRotation(s1Array, s2Array,start1+size-i,start2,size-i ))){
+					result = true;
+					break;
+				}
+		}
+		return result;	
+	}
+
+
+	public static void main(String[] args){
+		String str1 = "dbac";	
+		String str2 = "abcd";	
+		System.out.println(isRotation(str1,str2));
+	
+	}
+}
