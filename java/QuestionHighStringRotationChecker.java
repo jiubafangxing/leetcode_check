e284b60a1c645dc981ded1328860f7415928908a
yunrong <18404903536@163.com>
2024-07-30 08:59:11 +0800

    QuestionHighStringRotationChecker

--- java/QuestionHighStringRotationChecker.java
+++ java/QuestionHighStringRotationChecker.java
@@ -4,45 +4,41 @@
  */
 public class QuestionHighStringRotationChecker {
 
-	public static boolean isRotation(String str1, String str2){
-		if(str1 == null || str2 == null || str1.length() != str2.length()){
+	public static boolean isRotation(String str1, String str2) {
+		if (str1 == null || str2 == null || str1.length() != str2.length()) {
 			return false;
 		}
 		char[] s1Array = str1.toCharArray();
 		char[] s2Array = str2.toCharArray();
 
-		return isRotation(s1Array,s2Array,0,0, s1Array.length);
+		return isRotation(s1Array, s2Array, 0, 0, s1Array.length);
 	}
 
-
-	public static boolean isRotation2(String str1, String str2){
-		if(str1 == null || str2 == null || str1.length() != str2.length()){
+	public static boolean isRotation2(String str1, String str2) {
+		if (str1 == null || str2 == null || str1.length() != str2.length()) {
 			return false;
 		}
 		char[] s1Array = str1.toCharArray();
 		char[] s2Array = str2.toCharArray();
 		int N = s1Array.length;
-		boolean[][][] dp = new boolean[N+1][N][N];
-		for(int i=0;i< N;i++){
-			for(int j=0;j< N;j++){
-				dp[1][i][j] = (s1Array[i]  == s2Array[j]);
+		boolean[][][] dp = new boolean[N + 1][N][N];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				dp[1][i][j] = (s1Array[i] == s2Array[j]);
 			}
 		}
-		//still need to adjust
-		for(int i=2;i< dp.length;i++){
-			for(int start1=1;start1<= N-i;start1++){
-				for(int start2=1;start2<= N-i;start2++){
-					for(int leftPart=1;leftPart< i; leftPart++){
-					if(
-						(dp[leftPart][start1][start2] && dp[i-leftPart][start1+i][start2+i])
-						||
-						
-						(dp[leftPart][start1][start2+N-i] && dp[i-leftPart][start1+i][start2])
+		for (int i = 2; i < dp.length; i++) {
+			for (int start1 = 0; start1 <= N - i; start1++) {
+				for (int start2 = 0; start2 <= N - i; start2++) {
+					for (int leftPart = 1; leftPart < i; leftPart++) {
+						if ((dp[leftPart][start1][start2] && dp[i - leftPart][start1 + leftPart][start2 + leftPart])
+								||
 
-					){
-						dp[i][start1][start2] = true;
-						break;
-					}
+								(dp[leftPart][start1][start2 + i - leftPart]
+										&& dp[i - leftPart][start1 + leftPart][start2])) {
+							dp[i][start1][start2] = true;
+							break;
+						}
 					}
 
 				}
@@ -52,41 +48,38 @@ public class QuestionHighStringRotationChecker {
 		return dp[N][0][0];
 	}
 
-	public static boolean check(char[] str1, char[] str2){
-		if(null == str1 || null == str2){
+	public static boolean check(char[] str1, char[] str2) {
+		if (null == str1 || null == str2) {
 			return false;
 		}
-		if(str1.length != str2.length){
+		if (str1.length != str2.length) {
 			return false;
 		}
 		return true;
 	}
 
-
-	public static boolean isRotation(char[] s1Array, char[] s2Array, int start1, int start2, int size){
-		if(size == 1){
+	public static boolean isRotation(char[] s1Array, char[] s2Array, int start1, int start2, int size) {
+		if (size == 1) {
 			return s1Array[start1] == s2Array[start2];
 		}
 		boolean result = false;
-		for(int i=1; i< size;i++){
-			if((isRotation(s1Array,s2Array, start1, start2,i)&& isRotation(s1Array,s2Array, start1+ i,start2+i, size-i)) 
-					||( isRotation(s1Array, s2Array, start1,start2+size-i,i ) && isRotation(s1Array, s2Array,start1+i,start2,size-i ))){
+		for (int i = 1; i < size; i++) {
+			if ((isRotation(s1Array, s2Array, start1, start2, i)
+					&& isRotation(s1Array, s2Array, start1 + i, start2 + i, size - i))
+					|| (isRotation(s1Array, s2Array, start1, start2 + size - i, i)
+							&& isRotation(s1Array, s2Array, start1 + i, start2, size - i))) {
 				result = true;
 				break;
 			}
 		}
-		return result;	
+		return result;
 	}
 
-
-
-
-
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		String str1 = "abcd";
 		String str2 = "cdab";
-		System.out.println(isRotation(str1,str2));
-		System.out.println(isRotation2(str1,str2));
+		System.out.println(isRotation(str1, str2));
+		System.out.println(isRotation2(str1, str2));
 
 	}
 }
