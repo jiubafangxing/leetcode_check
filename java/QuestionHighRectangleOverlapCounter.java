fd0789be124e3e1d3f83fce771589e5cc0b0ffe6
jiubafangxing <18404903536@163.com>
2024-07-31 22:15:54 +0800

    QuestionHighRectangleOverlapCounter

--- java/QuestionHighRectangleOverlapCounter.java
+++ java/QuestionHighRectangleOverlapCounter.java
@@ -4,11 +4,11 @@ import java.util.*;
 
  */
 public class QuestionHighRectangleOverlapCounter {
-	
+
 	public static class Rectangle{
-		
+
 		public 	int left;
-	
+
 		public int down;
 
 		public int right;
@@ -23,53 +23,102 @@ public class QuestionHighRectangleOverlapCounter {
 			this.top = top;	
 		}
 
+	
+
 	}
 
 	public static  class DownComparator  implements Comparator<Rectangle>{
 		@Override
-		public int compare(Rectangle r1, Rectangle r2){
-			return r1.down-r2.down;	
-		}	
+			public int compare(Rectangle r1, Rectangle r2){
+				return r1.down-r2.down;	
+			}	
 	}
 
 	public static class LeftComparator implements Comparator<Rectangle>{
 		@Override
-		public int compare(Rectangle r1, Rectangle r2){
-			return r1.left -r2.left;	
-		}	
+			public int compare(Rectangle r1, Rectangle r2){
+				return r1.left -r2.left;	
+			}	
 	}
 	public  static class RightComparator implements Comparator<Rectangle>{
 		@Override
-		public int compare(Rectangle r1, Rectangle r2){
-			return r1.right-r2.right;	
-		}	
+			public int compare(Rectangle r1, Rectangle r2){
+				return r1.right-r2.right;	
+			}	
 	}
 
 
 	public static int count(Rectangle[] rectangles){
-		int result = -1;
 		Arrays.sort(rectangles, new DownComparator());
-		TreeSet<Rectangle> leftRectanles = new TreeSet<Rectangle>();
-		for(Rectangle rectangle:rectangles){
+		TreeSet<Rectangle> leftRectangles = new TreeSet<Rectangle>(new LeftComparator());
+		int maxSize = 0;
+		for(int i=0;i< rectangles.length; i++){
+			Rectangle rectangle = rectangles[i];
+
+			int index = i;
+			int curDown = rectangle.down;
+			while(index< rectangles.length && rectangles[index].down == curDown){
+				leftRectangles.add(rectangles[index]);
+				index++;
+			}
 			clearLessThanBottom(leftRectangles, rectangle.down);	
+
+			i = index;
+
+			TreeSet<Rectangle> rights = new TreeSet<Rectangle>(new RightComparator());
+			for(Rectangle l :leftRectangles){
+				removeLessThanLeft(rights, l.left);
+				rights.add(l);
+				maxSize = Math.max(maxSize, rights.size());	
+			}
+
 		}
-		return result ;	
+		return maxSize;	
+	}
+
+
+	public static void removeLessThanLeft(TreeSet<Rectangle> t, int l){
+		if(null == t || t.size() == 0){
+			return;
+		}
+		List<Rectangle> removes = new ArrayList();
+		for(Rectangle r:t){
+			if(r.right <= l){
+				removes.add(r);					
+			}else{
+				break;
+			}	
+		}
+		for(Rectangle r:removes){
+			t.remove(r);
+		}
 	}
 
 
 	public static void clearLessThanBottom(TreeSet<Rectangle> t, int d ){
+		if(null == t || t.size() == 0){
+			return;
+		}
 		List<Rectangle> removes = new ArrayList();
 		for(Rectangle r:t){
-			if(r.top >= d){
-				break;
-			}else{
-					
-			}	
-		}	
-	
+			if(r.top < d){
+				removes.add(r);
+			}
+		}
+		for(Rectangle r:removes){
+			t.remove(r);
+		}
+
 	}
 	public static void main(String[] args){
-	
-	
+		Rectangle[] testCase5 = {
+			 new Rectangle(1, 3, 2, 6),
+            new Rectangle(1, 3,2, 6),
+            new Rectangle(1, 3, 2, 6)
+		};
+		int result = count(testCase5);
+		System.out.println("result is "+ result);
+
+
 	}
 }
